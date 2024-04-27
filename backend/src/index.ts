d6a375e19d3d8bc8964dfcae589fc67ccc4e17cb
ab7022 <bayees1@gmail.com>
2024-04-28 05:18:03 +0530

    added frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,16 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import {decode,sign, verify} from 'hono/jwt'
-import { blogRouter } from './routes/blog'
-import { userRouter } from './routes/user'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors'
+
 const app = new Hono<{
-  Bindings:{
-    DATABASE_URL:string
-    JWT_SECRET:string
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
   }
-}>()
- 
-
-app.route("/api/v1/blog",blogRouter)
-app.route("/api/v1/user",userRouter)
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
+}>();
+app.use('/*', cors())
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = 'https://backend.bayees1.workers.dev'
